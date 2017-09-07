4906dccb44209c2763028e0ef6dfb6a343b665a8
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-09-07 16:29:34 -0300

    Guarda series como variaveis de objeto

--- lib/minhas_series.rb
+++ lib/minhas_series.rb
@@ -1,39 +1,29 @@
 class MinhasSeries
 
+  attr_reader :series_assistir, :series_assistidas, :series_assistindo
+
   def initialize
-    @@series_assistindo = []
-    @@series_assistidas = []
-    @@series_assistir = []
+    @series_assistindo ||= []
+    @series_assistidas ||= []
+    @series_assistir ||= []
   end
 
   def adicionar_serie(status, serie)
     case status
     when 'assistindo'
-      if !@@series_assistindo.include?(serie)
-        if @@series_assistindo.size < 2
-          @@series_assistindo.push(serie)
+      if !@series_assistindo.include?(serie)
+        if @series_assistindo.size < 2
+          @series_assistindo.push(serie)
         end
       end
     when 'assistida'
-      if !@@series_assistindo.include?(serie)
-        @@series_assistidas.push(serie)
+      if !@series_assistindo.include?(serie)
+        @series_assistidas.push(serie)
       end
     when 'assistir'
-      if !@@series_assistir.include?(serie)
-        @@series_assistir.push(serie)
+      if !@series_assistir.include?(serie)
+        @series_assistir.push(serie)
       end
     end
-  end
-
-  def series_assistidas
-    @@series_assistidas
-  end
-
-  def series_assistindo
-    @@series_assistindo
-  end
-
-  def series_assistir
-    @@series_assistir
   end
 end
