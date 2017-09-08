040aa034c86e40f665e65dee776127649647265b
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-09-08 13:52:08 -0300

    simplifica lógica de validação de moeda

--- lib/cotacao_util.rb
+++ lib/cotacao_util.rb
@@ -10,9 +10,19 @@ class CotacaoUtil
     @@cotacao.keys
   end
 
+  def self.converter(dinheiro, moeda_conversao)
+    if moedas.include?(dinheiro.moeda) && moedas.include?(moeda_conversao)
+      valor_cotacao = @@cotacao[dinheiro.moeda][moeda_conversao]
+      preco = (dinheiro.valor * valor_cotacao)/100
+    end
+    raise ArgumentError.new("Não existe cotação de #{moeda_base} para #{moeda_conversao}")
+  end
+
   def self.converter(preco, moeda_base, moeda_conversao)
-    raise ArgumentError.new("Não existe cotação de #{moeda_base} para #{moeda_conversao}") unless moedas.include?(moeda_base) && moedas.include?(moeda_conversao)
-    valor_cotacao = @@cotacao[moeda_base][moeda_conversao]
-    preco = (preco * valor_cotacao)/100
+    if moedas.include?(moeda_base) && moedas.include?(moeda_conversao)
+      valor_cotacao = @@cotacao[moeda_base][moeda_conversao]
+      return (preco * valor_cotacao)/100
+    end
+    raise ArgumentError.new("Não existe cotação de #{moeda_base} para #{moeda_conversao}")
   end
 end
