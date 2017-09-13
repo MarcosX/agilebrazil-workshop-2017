480e60d79392217036c1a9a8a9f700049e9c069f
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-09-13 15:35:37 -0300

    Adiciona mais testes para serie

--- test/test_serie.rb
+++ test/test_serie.rb
@@ -2,12 +2,31 @@ require_relative './testing_helper'
 
 class TestSerie < Minitest::Test
 
-  def test_adiciona_preco_box
+  def test_mostra_preco_box_varias_moedas
     serie = Serie.new('The Office')
     serie.definir_preco(49990, 'BRL')
 
     assert_equal 'BRL 499.90', serie.preco_box('BRL')
     assert_equal 'USD 159.96', serie.preco_box('USD')
     assert_equal 'EUR 129.97', serie.preco_box('EUR')
+  end
+
+  def test_serie_sem_preco
+    serie = Serie.new('The Office')
+    assert_raises(ArgumentError) { serie.preco_box('BRL') }
+  end
+
+  def test_serie_com_moeda_invalida
+    serie = Serie.new('The Office')
+    serie.definir_preco(49990, 'BRT')
+
+    assert_raises(ArgumentError) { serie.preco_box('BRL') }
+  end
+
+  def test_preco_box_com_moeda_invalida
+    serie = Serie.new('The Office')
+    serie.definir_preco(49990, 'BRL')
+
+    assert_raises(ArgumentError) { serie.preco_box('USB') }
   end
 end
